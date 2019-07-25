784d458f1bc527d9dda6652f6121de66bb37fd31
오동훈 <odh3126.personal@gmail.com>
2019-07-25 18:06:26 +0900

    07/25

---
+++ 01_java/src/exam/Test.java
@@ -0,0 +1,10 @@
+package exam;
+
+public class Test {
+
+	public static void main(String[] args) {
+		new BoardUI().service();
+
+	}
+
+}
---
+++ 01_java/src/exam/ui/DetailBoardUI.java
@@ -0,0 +1,5 @@
+package exam.ui;
+
+public class DetailBoardUI {
+
+}
